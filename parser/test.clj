73e29bae8237560c78dc76852aa29bf64785e6b0
jenkins.333 <jenkins.333@gmail.com>
2009-06-25 02:13:02 +0000

    Added temporary function to show how to select a certain type of log entry.  Also added a temporary function to show how to select from join/quit messages for a specific person.

--- parser/test.clj
+++ parser/test.clj
@@ -11,3 +11,9 @@
     (def parse-data #{})
     (doseq [line (line-seq br)] (def parse-data (conj parse-data (parse line log-line))))
     parse-data))
+
+(defn get-connections [log-set]
+  (clojure.set/select #(connection? (:entry %)) log-set))
+
+(defn select-player-from-connections [connection-set name]
+  (clojure.set/select #(.equalsIgnoreCase name (:name (:player (:entry %)))) connection-set))
