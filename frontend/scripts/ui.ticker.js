680e283ebf2715d95329fe640ed756deaf11163f
Chris Ward <chrisw1229@users.noreply.github.com>
2009-07-24 06:13:40 +0000

    Updated player ticker content based on user feedback.

--- frontend/scripts/ui.ticker.js
+++ frontend/scripts/ui.ticker.js
@@ -19,19 +19,29 @@ $.widget("ui.ticker", {
       var photoDiv = $('<div class="ui-widget-content ui-ticker-item-photo"></div>').appendTo(contDiv);
       photoDiv.css("background-image", "url(players/" + item.photo + ")");
 
+      var iconsDiv = $('<div class="ui-ticker-item-icons"></div>').appendTo(contDiv);
+      $('<div class="ui-widget-header ui-corner-bl ui-ticker-place">'
+          + '<div class="ui-ticker-place-label">RANK</div>'
+          + '<div class="ui-ticker-place-value">' + item.p + '</div>'
+          + '</div>').appendTo(iconsDiv);
+      $('<div class="icon-rank icon-rank-' + item.r
+          + ' ui-ticker-item-rank"</div>').appendTo(iconsDiv);
+      $('<div class="icon-team icon-team-' + item.t
+          + ' ui-ticker-item-team"</div>').appendTo(iconsDiv);
+
       var stats = $('<table class="ui-ticker-stats">'
           + '<tr class="ui-ticker-stat-line1">'
             + '<td class="ui-ticker-stat-value">' + item.k + '</td>'
             + '<td>Kills</td>'
-            + '<td></td>'
           + '</tr><tr class="ui-ticker-stat-line2">'
             + '<td class="ui-ticker-stat-value">' + item.d + '</td>'
             + '<td>Deaths</td>'
-            + '<td><span class="icon-rank icon-rank-' + item.r + '"</span></td>'
           + '</tr><tr class="ui-ticker-stat-line2">'
             + '<td class="ui-ticker-stat-value">' + item.c + '</td>'
             + '<td>Change</td>'
-            + '<td><span class="icon-team icon-team-' + item.t + '"</span></td>'
+            + '<td/>'
+          + '</tr><tr class="ui-ticker-stat-line2">'
+            + '<td colspan="2">Best Enemy</td>'
           + '</tr></table>').appendTo(contDiv);
       var enemyDiv = $('<div class="ui-corner-bottom ui-ticker-item-footer">'
           + item.e + '</div>').appendTo(itemDiv);
@@ -54,10 +64,10 @@ $.extend($.ui.ticker, {
   version: "1.7.2",
   defaults: {
     items: [
-      { name: "A Figment of Your Imagination", photo: "player1.jpg", k: "79", d: "57", c: "+4", r: "3", t: "a", e: "GOMER PYLE" },
-      { name: "CHUCKNORRISCOUNTEDTOINFINITY...", photo: "player2.jpg", k: "27", d: "55", c: "+2", r: "1", t: "b", e: "ThePine" },
-      { name: "GOMER PYLE", photo: "player3.jpg", k: "108", d: "55", c: "-3", r: "4", t: "r", e: "CHUCKNORRISCOUNTEDTOINFINITY..." },
-      { name: "ThePine", photo: "player4.jpg", k: "144", d: "80", c: "+1", r: "5", t: "g", e: "A Figment of Your Imagination" }
+      { name: "A Figment of Your Imagination", photo: "player1.jpg", p: "7", r: "3", t: "a", k: "79", d: "57", c: "+4", e: "GOMER PYLE" },
+      { name: "CHUCKNORRISCOUNTEDTOINFINITY...", photo: "player2.jpg", p: "23", r: "1", t: "b", k: "27", d: "55", c: "+2", e: "ThePine" },
+      { name: "GOMER PYLE", photo: "player3.jpg", p: "2", r: "4", t: "r", k: "108", d: "55", c: "-3", e: "CHUCKNORRISCOUNTEDTOINFINITY..." },
+      { name: "ThePine", photo: "player4.jpg", p: "1", r: "5", t: "g", k: "144", d: "80", c: "+1", e: "A Figment of Your Imagination" }
     ]
   }
 });
