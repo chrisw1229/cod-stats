b6484c496465d98e1322b82babe35f2b0d6d7ba2
Chris Ward <chrisw1229@users.noreply.github.com>
2009-07-27 03:52:18 +0000

    Updated player ticker content based on user feedback.

--- frontend/scripts/ui.ticker.js
+++ frontend/scripts/ui.ticker.js
@@ -15,36 +15,36 @@ $.widget("ui.ticker", {
       var shadowDiv = $('<div class="ui-widget-shadow ui-ticker-item-shadow"></div>').appendTo(itemDiv);
       var headerDiv = $('<div class="ui-state-default ui-corner-top ui-ticker-item-header">'
           + item.name + '</div>').appendTo(itemDiv);
-      var contDiv = $('<div class="ui-widget-content ui-ticker-item-content"></div>').appendTo(itemDiv);
-      var photoDiv = $('<div class="ui-widget-content ui-ticker-item-photo"></div>').appendTo(contDiv);
+      var contDiv = $('<div class="ui-widget-content ui-corner-bottom ui-ticker-item-content"></div>').appendTo(itemDiv);
+      var photoDiv = $('<div class="ui-widget-content ui-corner-br ui-ticker-item-photo"></div>').appendTo(contDiv);
       photoDiv.css("background-image", "url(players/" + item.photo + ")");
 
       var iconsDiv = $('<div class="ui-ticker-item-icons"></div>').appendTo(contDiv);
       $('<div class="ui-widget-header ui-corner-bl ui-ticker-place">'
           + '<div class="ui-ticker-place-label">RANK</div>'
-          + '<div class="ui-ticker-place-value">' + item.p + '</div>'
+          + '<div class="ui-ticker-place-value">' + item.place + '</div>'
           + '</div>').appendTo(iconsDiv);
-      $('<div class="icon-rank icon-rank-' + item.r
+      $('<div class="icon-rank icon-rank-' + item.rank
           + ' ui-ticker-item-rank"</div>').appendTo(iconsDiv);
-      $('<div class="icon-team icon-team-' + item.t
+      $('<div class="icon-team icon-team-' + item.team
           + ' ui-ticker-item-team"</div>').appendTo(iconsDiv);
 
       var stats = $('<table class="ui-ticker-stats">'
           + '<tr class="ui-ticker-stat-line1">'
-            + '<td class="ui-ticker-stat-value">' + item.k + '</td>'
+            + '<td class="ui-ticker-stat-value">' + item.kills + '</td>'
             + '<td>Kills</td>'
           + '</tr><tr class="ui-ticker-stat-line2">'
-            + '<td class="ui-ticker-stat-value">' + item.d + '</td>'
+            + '<td class="ui-ticker-stat-value">' + item.deaths + '</td>'
             + '<td>Deaths</td>'
-          + '</tr><tr class="ui-ticker-stat-line2">'
-            + '<td class="ui-ticker-stat-value">' + item.c + '</td>'
-            + '<td>Change</td>'
+          + '</tr><tr class="ui-ticker-stat-line3">'
+            + '<td class="ui-ticker-stat-value">' + item.inflicted + '</td>'
+            + '<td>Inflicted</td>'
             + '<td/>'
-          + '</tr><tr class="ui-ticker-stat-line2">'
-            + '<td colspan="2">Best Enemy</td>'
+          + '</tr><tr class="ui-ticker-stat-line3">'
+            + '<td class="ui-ticker-stat-value">' + item.received + '</td>'
+            + '<td>Received</td>'
+            + '<td/>'
           + '</tr></table>').appendTo(contDiv);
-      var enemyDiv = $('<div class="ui-corner-bottom ui-ticker-item-footer">'
-          + item.e + '</div>').appendTo(itemDiv);
     }
   },
 
@@ -64,10 +64,10 @@ $.extend($.ui.ticker, {
   version: "1.7.2",
   defaults: {
     items: [
-      { name: "A Figment of Your Imagination", photo: "player1.jpg", p: "7", r: "3", t: "a", k: "79", d: "57", c: "+4", e: "GOMER PYLE" },
-      { name: "CHUCKNORRISCOUNTEDTOINFINITY...", photo: "player2.jpg", p: "23", r: "1", t: "b", k: "27", d: "55", c: "+2", e: "ThePine" },
-      { name: "GOMER PYLE", photo: "player3.jpg", p: "2", r: "4", t: "r", k: "108", d: "55", c: "-3", e: "CHUCKNORRISCOUNTEDTOINFINITY..." },
-      { name: "ThePine", photo: "player4.jpg", p: "1", r: "5", t: "g", k: "144", d: "80", c: "+1", e: "A Figment of Your Imagination" }
+      { name: "A Figment of Your Imagination", photo: "player1.jpg", place: "7", rank: "3", team: "a", kills: "79", deaths: "57", inflicted: "500", received: "200" },
+      { name: "CHUCKNORRISCOUNTEDTOINFINITY...", photo: "player2.jpg", place: "23", rank: "1", team: "b", kills: "27", deaths: "55", inflicted: "65", received: "1,456" },
+      { name: "GOMER PYLE", photo: "player3.jpg", place: "2", rank: "4", team: "r", kills: "108", deaths: "55", inflicted: "800", received: "80" },
+      { name: "ThePine", photo: "player4.jpg", place: "1", rank: "5", team: "g", kills: "144", deaths: "80", inflicted: "2,123", received: "1,789" }
     ]
   }
 });
