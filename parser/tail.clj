c8b2e4311225ae0562368919cdb5b3ee2669af1f
jenkins.333 <jenkins.333@gmail.com>
2009-09-10 02:10:26 +0000

    tail-f function fixed to use threads properly now.  implemented using a closure to close over arguments passed in and pass back an object with the :start and :stop functions.

--- parser/tail.clj
+++ parser/tail.clj
@@ -1,16 +1,13 @@
 (defn tail-f [file delay action]
-  (let [keep-running (atom true)]
-    {:start #(with-open [fr (java.io.FileReader. file)
-			 br (java.io.BufferedReader. fr)]
-	       (while @keep-running
-		 (let [current-line (.readLine br)]
-		   (if (not (nil? current-line))
-		     (action current-line)
-		     (Thread/sleep delay)))))
-     :stop #(reset! keep-running false)}))
-
-(defn watch-file [file]
-  (let [file-watcher (tail-f file 1000 println)
-	thread (Thread. #(((file-watcher :start))))]
-    (.start thread)))
-;Figure out how to return the file-watcher so I can call stop later
+  (let [keep-running (atom true)
+	thread (Thread. #(with-open [fr (java.io.FileReader. file)
+				     br (java.io.BufferedReader. fr)]
+			   (while @keep-running
+			     (let [current-line (.readLine br)]
+			       (if (not (nil? current-line))
+				 (action current-line)
+				 (Thread/sleep delay))))))]
+    {:start #(.start thread)
+     :stop #(do (reset! keep-running false)
+		(.join thread)
+		(println "tail-f has stopped."))}))
