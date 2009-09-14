f935ee849a7199731d6014e5f73227948c0e4a7b
jenkins.333 <jenkins.333@gmail.com>
2009-09-14 01:30:32 +0000

    Refactored analytics into its own file.
    Moved file watching into test.clj.  Updated to be more robust and to not use buffered input.
    
    Updated setup.clj to read a smaller log file.

--- parser/setup.clj
+++ parser/setup.clj
@@ -1,5 +1,5 @@
 (add-classpath "file:///c:/projects/codParser/")
 (load-file "c:/Projects/codParser/test.clj")
 
-(def p-data (parse-log "C:/Projects/codParser/logs/games_mp_gday10_raw.log"))
+(def p-data (parse-log "C:/Projects/codParser/short-log.log"))
 (def dk-seq (get-log-type p-data damage-kill?))
